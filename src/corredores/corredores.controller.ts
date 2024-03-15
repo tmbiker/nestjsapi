ec98834af7fc2e464fbbebbe0a68564484d220f3
TMBIkere <trasmontania@gmail.comm>
2024-03-14 21:12:21 -0300

    nuevoCorredor08

--- src/corredores/corredores.controller.ts
+++ src/corredores/corredores.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe, Post, Body, Res } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, Body} from '@nestjs/common';
 import { CorredoresService } from './corredores.service';
 import { NuevoCorredoresDto }  from './dto/nuevocorredores.dto'
 
@@ -6,12 +6,18 @@ import { NuevoCorredoresDto }  from './dto/nuevocorredores.dto'
 @Controller('corredores')
 export class CorredoresController {
 
-    constructor(private corredoresService: CorredoresService){}
+    constructor(private corredoresService: CorredoresService){ 
+
+    }
 
     @Post('/nuevo')
-    nuevoCorredores(@Body() nuevoCorredores: NuevoCorredoresDto){
-       return this.corredoresService.nuevoCorredores(nuevoCorredores)
+    create(): string {
+        return 'This action adds a new cat';
     }
+
+    //async nuevoCorredores(@Body() nuevoCorredores: NuevoCorredoresDto){
+    //   return this.corredoresService.nuevoCorredores(nuevoCorredores)
+    //}
     
     @Get('buscar/:doc_numero')
     getCorredores(@Param('doc_numero', ParseIntPipe) doc_numero: number){
