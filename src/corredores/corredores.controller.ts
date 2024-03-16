5b885e50dd3f66ba47752fa1024c96351b9744da
TMBIkere <trasmontania@gmail.comm>
2024-03-16 10:25:11 -0300

    nuevoCorredores17

--- src/corredores/corredores.controller.ts
+++ src/corredores/corredores.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, Param, Body, Post, Put, Header, Get, ParseIntPipe, ValidationPipe, UsePipes} from '@nestjs/common';
 import { CorredoresService } from './corredores.service';
-import { NuevoCorredoresDto }  from './dto/nuevocorredores.dto'
-
+import { NuevoCorredoresDto }  from './dto/nuevocorredores.dto';
 
 @Controller('corredores')
 export class CorredoresController {
-
     constructor(private readonly corredoresService: CorredoresService){ 
 
     }
