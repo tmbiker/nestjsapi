238d2511d5c3937d9ebbb55b8a1caa03f8382c92
TMBIkere <trasmontania@gmail.comm>
2024-03-16 10:50:25 -0300

    corredorNuevo19

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow('PORT');
   
-  app.enableCors({
-        "origin": "*",
-        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
-  });
-
+  
   app.setGlobalPrefix('/api/v1/');
   app.useGlobalPipes(new ValidationPipe());
 
