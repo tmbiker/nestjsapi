db9e7c6f82c3a0dbd4982074fa1bf07a16dae454
TMBIkere <trasmontania@gmail.comm>
2024-03-16 08:55:59 -0300

    nuevoCorredor14

--- src/corredores/corredores.controller.ts
+++ src/corredores/corredores.controller.ts
@@ -10,14 +10,14 @@ export class CorredoresController {
 
     }
 
-    @Post('nuevo')
+    @Post('/nuevo/')
     @Header('Cache-Control', 'none')
     @UsePipes(ValidationPipe)
     async nuevoCorredores(@Body() nuevoCorredores: NuevoCorredoresDto){
        return this.corredoresService.nuevoCorredores(nuevoCorredores)
     }
 
-    @Put('modificar/:doc_numero')
+    @Put('/modificar/:doc_numero')
     @UsePipes(ValidationPipe)
     async modificarCorredores(@Param('doc_numero', ParseIntPipe) doc_numero: number, @Body() nuevoCorredores: NuevoCorredoresDto){
        return this.corredoresService.modificarCorredores(doc_numero, nuevoCorredores)
