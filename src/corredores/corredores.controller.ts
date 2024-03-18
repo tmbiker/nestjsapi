8b3bacb18ba3f94d74293dc72b8dd738862ceef6
TMBIkere <trasmontania@gmail.comm>
2024-03-18 09:06:39 -0300

    nuevoCorredor19

--- src/corredores/corredores.controller.ts
+++ src/corredores/corredores.controller.ts
@@ -8,3 +8,22 @@ const router = express.Router()
 router.post('/nuevo', (req, res) => {
     res.send('About Corredores')
 })
+
+@Controller('corredores')
+export class CorredoresController {
+    constructor(private readonly corredoresService: CorredoresService){ 
+
+    }
+
+    @Put('modificar/:doc_numero')
+    @UsePipes(ValidationPipe)
+    modificarCorredores(@Param('doc_numero', ParseIntPipe) doc_numero: number, @Body() nuevoCorredores: NuevoCorredoresDto){
+       return this.corredoresService.modificarCorredores(doc_numero, nuevoCorredores);
+    }
+
+    @Get('buscar/:doc_numero')
+    getCorredores(@Param('doc_numero', ParseIntPipe) doc_numero: number){
+        return this.corredoresService.getCorredores(doc_numero);
+    }
+
+}
