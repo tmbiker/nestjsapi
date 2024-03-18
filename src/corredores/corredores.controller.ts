20b6a6c993829872570618ec9d4680eff876d404
TMBIkere <trasmontania@gmail.comm>
2024-03-17 23:56:29 -0300

    corredorNuevo16

--- src/corredores/corredores.controller.ts
+++ src/corredores/corredores.controller.ts
@@ -2,22 +2,22 @@ import { Controller, Param, Body, Post, Put, Header, Get, ParseIntPipe, Validati
 import { CorredoresService } from './corredores.service';
 import { NuevoCorredoresDto }  from './dto/nuevocorredores.dto';
 
-@Controller('/corredores')
+@Controller('corredores')
 export class CorredoresController {
     constructor(private readonly corredoresService: CorredoresService){ 
 
     }
 
-    @Post('/nuevo')
+    @Post('nuevo')
     @Header('Cache-Control', 'none')
     @UsePipes(ValidationPipe)
-    async nuevoCorredores(@Body() nuevoCorredores: NuevoCorredoresDto){
+    nuevoCorredores(@Body() nuevoCorredores: NuevoCorredoresDto){
        return this.corredoresService.nuevoCorredores(nuevoCorredores);
     }
 
-    @Put('/modificar/:doc_numero')
+    @Put('modificar/:doc_numero')
     @UsePipes(ValidationPipe)
-    async modificarCorredores(@Param('doc_numero', ParseIntPipe) doc_numero: number, @Body() nuevoCorredores: NuevoCorredoresDto){
+    modificarCorredores(@Param('doc_numero', ParseIntPipe) doc_numero: number, @Body() nuevoCorredores: NuevoCorredoresDto){
        return this.corredoresService.modificarCorredores(doc_numero, nuevoCorredores);
     }
     
